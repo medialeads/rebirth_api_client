ae6550dab7ee25f6fb06a644125f597445fed271
Dagan MENEZ <daganmenez.dev@gmail.com>
2017-10-27 11:01:33 +0200

    add symfony ExpressionLanguage dependency

--- Helper/VariantHelper.php
+++ Helper/VariantHelper.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Helper;
+namespace ES\APIv2Client\Helper;
 
 class VariantHelper
 {
-    public static function getCalculatedPrice()
+    public static function getCalculatedPrice($variant)
     {
 
     }
---
+++ Model/CalculatedPrice.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ES\APIv2Client\Model;
+
+class CalculatedPrice
+{
+    /**
+     * @var float
+     */
+    private $value;
+
+    /**
+     * @return float
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param float $value
+     *
+     * @return CalculatedPrice
+     */
+    public function setValue($value)
+    {
+        $value = floatval($value);
+        if ($value <= 0.0) {
+            $value = null;
+        }
+
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param float $value
+     *
+     * @return CalculatedPrice
+     */
+    public function addValue($value)
+    {
+        $value = floatval($value);
+        if ($value > 0.0) {
+            if (null === $this->value) {
+                $this->value = 0.0;
+            }
+
+            $this->value += $value;
+        }
+
+        return $this;
+    }
+}
