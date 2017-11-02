971cbc6d21bb9a48a6497918b5c249ed03007866
Dagan MENEZ <daganmenez.dev@gmail.com>
2017-11-02 15:31:36 +0100

    add first search functions in Client

--- Client.php
+++ Client.php
@@ -11,7 +11,7 @@ use ES\APIv2Client\Transformer\ProductTransformer;
 
 class Client
 {
-    const URL = "http://extranet-rebirth.es.local/generator.php";
+    const URL = "http://apiv2.europeansourcing.com/api/search";
 
     /**
      * @var string
@@ -19,25 +19,97 @@ class Client
     private $key;
 
     /**
-     * @param string $key
+     * @var string
      */
-    public function __construct($key)
+    private $countryCode;
+
+    /**
+     * @param string $key
+     * @param string $countryCode
+     */
+    public function __construct($key, $countryCode)
     {
         $this->key = $key;
+        $this->countryCode = $countryCode;
     }
 
     /**
+     * @param array[] $handlers
+     * @param int $page
+     * @param int $offset
+     * @param int $limit
+     * @param string $sort_direction
+     *
      * @return array|mixed|null
      */
-    public function search()
+    public function searchProductsBy($handlers, $page = 1, $offset = 0, $limit = 20, $sort_direction = 'asc')
+    {
+        $params = array(
+            'page' => $page,
+            'offset' => $offset,
+            'lang' => 'fr',
+            'limit' => $limit,
+            'sort_direction' => $sort_direction,
+            'context' => array(
+                'country_code' => $this->countryCode
+            ),
+            'search_handlers' => $handlers
+        );
+
+        return $this->searchRequest($params);
+    }
+
+    /**
+     * @param $string
+     * @param int $page
+     * @param int $offset
+     * @param int $limit
+     * @param string $sort_direction
+     *
+     * @return array|mixed|null
+     */
+    public function searchProductsByQuery($string, $page = 1, $offset = 0, $limit = 20, $sort_direction = 'asc')
+    {
+        $params = array(
+            'page' => $page,
+            'offset' => $offset,
+            'lang' => 'fr',
+            'limit' => $limit,
+            'sort_direction' => $sort_direction,
+            'context' => array(
+                'country_code' => $this->countryCode
+            ),
+            'search_handlers' => array(
+                array(
+                    'query' => $string
+                )
+            )
+        );
+
+        return $this->searchRequest($params);
+    }
+
+    /**
+     * @param array $params
+     *
+     * @return array|mixed|null
+     */
+    private function searchRequest($params)
     {
         $ch = curl_init(self::URL);
+        $param = json_encode($params);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/ld+json',
+            'Accept: application/ld+json',
+            'X-AUTH-TOKEN: ' .  $this->key
+        ));
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $param);
         curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
         $data = curl_exec($ch);
 
-        if (curl_errno($ch))
-        {
+        if (false === $data) {
             echo 'Erreur Curl : ' . curl_error($ch);
+            die();
         }
 
         curl_close($ch);
--- index.php
+++ index.php
@@ -2,17 +2,12 @@
 
 namespace ES\APIv2Client;
 
-use ES\APIv2Client\Model\Product;
-
 require_once __DIR__.'/vendor/autoload.php';
 
-$client = new Client("lol");
-$products = $client->search();
-foreach ($products as $product) {
-    /** @var Product $product */
-    if (!empty($product->getVariants())) {
-        foreach ($product->getVariants() as $variant) {
-            $client->price($variant);
-        }
-    }
-}
+$client = new Client("7hQ87N5c2Ts5XKlEYGRihrPc0FMaYmm0", 'fr');
+$handlers = array(
+    array(
+        'query' => 'sac'
+    )
+);
+$products = $client->searchProductsBy($handlers);
